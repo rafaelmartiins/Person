9e3382b0ebcd5be513fc3d65a5026c9736b7c0a2
Rafael Martins <80222726+rafaelmartiins@users.noreply.github.com>
2025-03-01 15:16:46 -0300

    Initial commit

---
+++ Data/PersonContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Person.Models;
+
+namespace Person.Data
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<PersonModel> People {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=person.Sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
